279b0e9a6b2cc6b34023aef97f308be8e0e2dd14
Maxim <toper.one111@gmail.com>
2024-09-14 17:05:49 +0300

    feat: add builder for input config

--- lab1/src/InputConfig.java
+++ lab1/src/InputConfig.java
@@ -23,4 +23,29 @@ public class InputConfig<T> {
     public String getPromptMessage() {
         return promptMessage;
     }
+
+    public static class Builder<T> {
+        private Supplier<T> inputSupplier;
+        private  Predicate<T> validator = _ -> true;
+        private String promptMessage = "Enter input: ";
+
+        public Builder<T> withInputSupplier(Supplier<T> inputSupplier) {
+            this.inputSupplier = inputSupplier;
+            return this;
+        }
+
+        public Builder<T> withValidator(Predicate<T> validator) {
+            this.validator = validator;
+            return this;
+        }
+
+        public Builder<T> withPromptMessage(String promptMessage) {
+            this.promptMessage = promptMessage;
+            return this;
+        }
+
+        public InputConfig<T> build() {
+            return new InputConfig<>(inputSupplier, validator, promptMessage);
+        }
+    }
 }
--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        InputConfig<Integer> inputConfig = new InputConfig<>(
-                scanner::nextInt,
-                input -> input > 1,
-                "Please enter an integer greater than 1: "
-        );
+        InputConfig<Integer> inputConfig = new InputConfig.Builder<Integer>()
+                .withInputSupplier(scanner::nextInt)
+                .withValidator(input -> input > 1)
+                .withPromptMessage("Please enter an integer greater than 1: ")
+                .build();
 
         int userInput = InputUtility.getInput(scanner, inputConfig);
 
