ae2bc7a585a2f862caa60b010924765ebe0b5198
Maxim <toper.one111@gmail.com>
2024-09-14 16:34:52 +0300

    feat: make get input method more generic

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,22 +1,28 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int userInput = getIntegerInput(scanner);
+        int userInput = getInput(scanner, scanner::nextInt, input -> true, "Please enter an integer: ");
         System.out.println("You entered: " + userInput);
     }
 
-    public static int getIntegerInput(Scanner scanner) {
-        int input = 0;
+    public static <T> T getInput(Scanner scanner, Supplier<T> inputSupplier, Predicate<T> validator, String promptMessage) {
+        T input = null;
         boolean validInput = false;
 
         while (!validInput) {
-            System.out.print("Please enter an integer: ");
+            System.out.print(promptMessage);
             try {
-                input = scanner.nextInt();
-                validInput = true;
+                input = inputSupplier.get();
+                if (validator.test(input)) {
+                    validInput = true;
+                } else {
+                    System.out.println("Invalid input. Does not meet the validation criteria.");
+                }
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please try again.");
                 scanner.next();
