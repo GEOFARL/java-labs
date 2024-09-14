7a9e3ca14ef0fc409332268eeac9891c8b65823f
Maxim <toper.one111@gmail.com>
2024-09-14 17:32:00 +0300

    feat: add cli arguments to handle different program modes

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java Main <mode>");
+            System.out.println("Modes: 'procedural' or 'functional'");
+            return;
+        }
+
         Scanner scanner = new Scanner(System.in);
+        String mode = args[0];
 
         InputConfig<Integer> inputConfig = new InputConfig.Builder<Integer>()
                 .withInputSupplier(scanner::nextInt)
@@ -10,10 +17,14 @@ public class Main {
                 .withPromptMessage("Please enter an integer greater than 1: ")
                 .build();
 
-        int userInput = InputUtility.getInputProcedural(scanner, inputConfig);
-        System.out.println("You entered: " + userInput);
-
-        userInput = InputUtility.getInputFunctional(scanner, inputConfig);
-        System.out.println("You entered: " + userInput);
+        if ("procedural".equalsIgnoreCase(mode)) {
+            int userInput = InputUtility.getInputProcedural(scanner, inputConfig);
+            System.out.println("Procedural: You entered: " + userInput);
+        } else if ("functional".equalsIgnoreCase(mode)) {
+            int userInput = InputUtility.getInputFunctional(scanner, inputConfig);
+            System.out.println("Functional: You entered: " + userInput);
+        } else {
+            System.out.println("Invalid mode. Please use 'procedural' or 'functional'.");
+        }
     }
 }
