d7fe4deb76121f09e6244c54d469678b2b4a18f4
Maxim <toper.one111@gmail.com>
2024-09-14 16:12:07 +0300

    feat: get integer input from user

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userInput = getIntegerInput(scanner);
+        System.out.println("You entered: " + userInput);
+    }
 
+    public static int getIntegerInput(Scanner scanner) {
+        int input = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("Please enter an integer: ");
+            try {
+                input = scanner.nextInt();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please try again.");
+                scanner.next();
+            }
+        }
+
+        return input;
     }
 }
